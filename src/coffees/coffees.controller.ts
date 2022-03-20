d510024538790ffc308809d7354c3c305affabdb
Abel Antonio Cerda <abelantoniocerda@gmail.com>
2022-03-20 00:15:48 -0300

    Add actions to update and remove

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Res } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'This action returns all coffees';
+  findAll(@Res() response) {
+    response.status(200).send('This action returns all coffees');
   }
 
   @Get(':id')
@@ -16,5 +16,15 @@ export class CoffeesController {
   create(@Body() body) {
     return body;
     // This action creates a coffee
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
